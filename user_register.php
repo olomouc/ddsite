4acd7302b3647921d551112ace6d96033c3d7936
olomouc <32953766+olomouc@users.noreply.github.com>
2017-10-22 09:50:12 +0100

    user register page

---
+++ user_register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="theme/css/style.css" media="all" />
+        <title>Register as a new user</title>
+    </head>
+    <body>
+        <?php
+            include 'core/init.php';
+        ?>
+        
+        
+        <form name="register" action="register.php" method="POST">
+            <input type="text" name="username" value="" />
+            <input type="password" name="Password" value="" />
+            <input type="submit" value="Register" name="register" />
+        </form>
+       
+</html>
